f59f102ab4db43bd8494e629f4dfa5a58c0d19d9
Seri-92 <64955102+serine-92@users.noreply.github.com>
2021-06-08 00:15:33 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+s  = input('時間を教えてください:')
+x, y = s.split(':')
+x = int(x)
+y = int(y)
+t = x * 60 + y
+t_700w = t * 5 // 7
+ans_m, ans_s = divmod(t_700w, 60)
+print(str(ans_m) + ':' + f'{ans_s:02}')
